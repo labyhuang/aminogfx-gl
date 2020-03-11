0104ecd27996f9ecc735d79154d0e38bedf9a718
laby <laby0916@gmail.com>
2020-03-11 09:35:14 +0800

    increase texture atlas size for chinese font

--- src/fonts.cpp
+++ src/fonts.cpp
@@ -176,7 +176,7 @@ void AminoFont::preInit(Nan::NAN_METHOD_ARGS_TYPE info) {
     this->fontData.Reset(bufferObj);
 
     //create atlas
-    atlas = texture_atlas_new(512, 512, 1); //depth must be 1
+    atlas = texture_atlas_new(2048, 2048, 1); //depth must be 1
 
     if (!atlas) {
         Nan::ThrowTypeError("could not create atlas");
